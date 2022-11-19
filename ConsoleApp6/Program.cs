ced891b0dbc376d8caff7d67766bfd6dab04ef0d
ShravanMCAnu <bskmc850@gmail.com>
2022-11-19 16:08:11 +0530

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeClassLibraray;
+
+EmployeeSalary objEmployeeSalary=new EmployeeSalary();
+objEmployeeSalary.GetEmployeeDetails();
+objEmployeeSalary.TaxCalculation();
+objEmployeeSalary.DisplayEmployeeDetails();
+objEmployeeSalary.DisplaySalaryDetails();
+
